1f465d8636e630f5fdf777509a7672f467cff360
Peter Shoaf <32045721+DownsizeEngineering@users.noreply.github.com>
2020-06-02 12:26:21 -0700

    add /hi route using get macro

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder, get};
 #[actix_rt::main]
 async fn main() -> std::io::Result<()>{
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(index))
             .route("/universe", web::get().to(index2))
+            .service(index3)
     })
     .bind("127.0.0.1:8888")?
     .run()
@@ -16,4 +17,9 @@ async fn index() -> impl Responder {
 }
 async fn index2() -> impl Responder {
     HttpResponse::Ok().body("Hello Universe!")
+}
+
+#[get("/hi")]
+async fn index3() -> impl Responder {
+    HttpResponse::Ok().body("Hi user!")
 }
