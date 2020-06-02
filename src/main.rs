4ff3d9aea78b66e2c1470db2877a981279ac0e08
Peter Shoaf <32045721+DownsizeEngineering@users.noreply.github.com>
2020-06-02 14:48:55 -0700

    added AppState to store app_name to display on app/index.html

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder, get};
 use listenfd::ListenFd;
+use std::sync::{Mutex};
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()>{
     let mut listenfd = ListenFd::from_env();
     let mut server = HttpServer::new(|| {
-        App::new().service(
-            web::scope("/app").route("/index.html", web::get().to(index)),
-        )
+        App::new()
+            .data(AppState{
+                app_name: String::from("lights_camera_actix"),
+            })
+            .service(
+                web::scope("/app").route("/index.html", web::get().to(index))
+                .service(index3),
+            )
     });
 
     server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
@@ -18,8 +24,13 @@ async fn main() -> std::io::Result<()>{
     server.run().await
 }
 
-async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello World!")
+struct AppState {
+    app_name: String,
+}
+
+async fn index(data: web::Data<AppState>) -> impl Responder {
+    let app_name = &data.app_name;
+    HttpResponse::Ok().body(format!("Hello {}!", app_name))
 }
 async fn index2() -> impl Responder {
     HttpResponse::Ok().body("Hello Universe!")
