dc6e19c93c69e23aa01e6eb7ce2bae2cf0ff5aca
Peter Shoaf <32045721+DownsizeEngineering@users.noreply.github.com>
2020-06-09 20:09:06 -0700

    added names route to fetch an entry using actix-postgres

--- src/main.rs
+++ src/main.rs
@@ -29,6 +29,7 @@ async fn main() -> std::io::Result<()>{
             )
             .configure(request_count::config_request_count)
             .route("hello/{name}/{greeting}", web::get().to(url_parser))
+            .route("names", web::get().to(names))
             .service(fs::Files::new("/", "./client/public")
                 .index_file("index.html")
             )
@@ -89,8 +90,31 @@ struct DBCredentials {
     password: String,
 }
 
-async fn pg () {
+async fn pg () -> String{
     let db_cred: DBCredentials = serde_json::from_str(
         &std::fs::read_to_string("./secrets.json").unwrap()
     ).unwrap();
+
+    let db_url = format!("postgresql://{}:{}@{}", 
+        db_cred.name, db_cred.password, db_cred.address);
+
+    let pg_actor = PostgresActor::start(&db_url, NoTls).unwrap(); 
+    let task: PostgresMessage<_, NoTls, _> = PostgresMessage::new(|pool| {
+        Box::pin(async move {
+            let connection = pool.get().await?;
+            connection
+                .query("SELECT * FROM names", &vec![])
+                .await
+                .map_err(|err| err.into())
+        })
+    });
+    let res = pg_actor.send(task).await.unwrap().unwrap();
+    let val:&str = res[0].get(0);
+    val.to_string()
+}
+
+async fn names() -> impl Responder {
+    let val = pg().await;
+    println!("pg got {}", val);
+    HttpResponse::Ok().body("names")
 }
