2432f5c698167acc65156e147a19d3a6e235f265
Peter Shoaf <32045721+DownsizeEngineering@users.noreply.github.com>
2020-06-10 19:59:22 -0700

    refactor pg into pg_query to accept &str input

--- src/db.rs
+++ src/db.rs
@@ -1,9 +1,8 @@
 use actix::prelude::*;
-use actix_postgres::{bb8_postgres::tokio_postgres::tls::NoTls,
-    PostgresActor, PostgresMessage
-};
+use actix_postgres::{PostgresActor, PostgresMessage};
 use actix_web::{HttpResponse, Responder};
 use serde::Deserialize;
+use actix_postgres::bb8_postgres::tokio_postgres::{tls::NoTls, row::Row};
 
 #[derive(Deserialize)]
 struct DBCredentials {
@@ -23,22 +22,36 @@ pub fn get_actor() -> Addr<PostgresActor<NoTls>> {
     PostgresActor::start(&db_url, NoTls).unwrap() 
 }
 
-async fn pg () -> String{
+async fn pg_query (query: &str) -> Vec<Row> {
+    let q = String::from(query);
     let task: PostgresMessage<_, NoTls, _> = PostgresMessage::new(|pool| {
         Box::pin(async move {
             let connection = pool.get().await?;
             connection
-                .query("SELECT * FROM names", &vec![])
+                .query(q.as_str(), &vec![])
                 .await
                 .map_err(|err| err.into())
         })
     });
     let res = get_actor().send(task).await.unwrap().unwrap();
-    let val:&str = res[0].get(0);
-    val.to_string()
+    // let val:&str = res[0].get(0);
+    // val.to_string()
+    res
+}
+
+
+struct User {
+    name: String,
+    id: i32,
 }
 
 pub async fn names() -> impl Responder {
-    let val = pg().await;
-    HttpResponse::Ok().body(val)
+    let res = pg_query("SELECT * FROM names").await;
+
+    let val= User {
+        name: res[0].get(0),
+        id: res[0].get(1),
+    };
+
+    HttpResponse::Ok().body(format!("{} {}", val.name, val.id))
 }
