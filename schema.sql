d8aa19b7d8ca12971983ef832b7dda66332bef53
Peter Shoaf <32045721+DownsizeEngineering@users.noreply.github.com>
2020-06-18 12:46:59 -0700

    restrict schema so that names must be present for todos and lists

--- schema.sql
+++ schema.sql
@@ -2,16 +2,16 @@ CREATE SCHEMA done;
 
 CREATE TABLE done.lists (
     id SERIAL NOT NULL,
-    name VARCHAR(100),
+    name VARCHAR(100) NOT NULL,
     PRIMARY KEY (id) 
 );
 
 CREATE TABLE done.todos (
     id SERIAL NOT NULL,
-    task VARCHAR(100),
+    task VARCHAR(100) NOT NULL,
     details VARCHAR(255),
     completed BOOLEAN NOT NULL,
-    list INTEGER,
+    list INTEGER NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (list) REFERENCES done.lists (id)
 );
