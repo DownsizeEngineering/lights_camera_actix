39a9a68a92259eb1124389aa244a3781cbcf622a
Peter Shoaf <32045721+DownsizeEngineering@users.noreply.github.com>
2020-06-22 19:53:19 -0700

    use fetch to query api, update spacing

--- client/src/components/App.svelte
+++ client/src/components/App.svelte
@@ -1,15 +1,31 @@
 <script>
-    import List from './List.svelte';
-    import NewList from './NewList.svelte';
-    let string = "Hello World"
-    import("../wasm/hello_wasm.js")
-        .then(module => {
-            module.greet();
-            string = "What a nice day!"
-        }
-    );
+  import List from './List.svelte';
+  import NewList from './NewList.svelte';
+  let string = "Hello World"
+  import("../wasm/hello_wasm.js")
+    .then(module => {
+      module.greet();
+      string = "What a nice day!"
+    }
+  );
 
-    let lists = [{"id":1,"name":"shopping","tasks":[{"id":1,"task":"eggs","details":null,"completed":false}]},{"id":2,"name":"banana","tasks":[]},{"id":3,"name":"banana","tasks":[]}];
+  let lists = [
+    {
+      "id":1,
+      "name":"shopping",
+      "tasks":[
+        {
+          "id":1,
+          "task":"eggs",
+          "details":null,
+          "completed":false
+        }
+      ]
+    }
+  ];
+  const td_utf8 = new TextDecoder("utf-8");
+  fetch("/list").then((res) => (res.body.getReader().read()))
+  .then((res) => {lists = JSON.parse(td_utf8.decode(res.value));});
 </script>
 <p>{string}</p>
 
