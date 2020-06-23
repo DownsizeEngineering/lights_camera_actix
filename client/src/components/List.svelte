1aaa4529943c42015c67aacce445c737d47d287c
Peter Shoaf <32045721+DownsizeEngineering@users.noreply.github.com>
2020-06-22 20:06:35 -0700

    use a blank string instead of null for blank details, encode parameters directly, add completed

--- client/src/components/List.svelte
+++ client/src/components/List.svelte
@@ -7,14 +7,17 @@
     if (task === "") return;
     let todo = {
       task,
-      details: details ? details : null,
+      details: details ? details : "",
     }
     list.tasks = [...list.tasks, todo];
-    let uri = `/list/${list.id}?id=0&task=${encodeURIComponent(todo.task)}`;
-    if (details) uri = uri + `&details=${encodeURIComponent(todo.details)}`;
+
+    let uri = `/list/${list.id}?id=0&task=${encodeURIComponent(task)}`;
+    if (details) uri += `&details=${encodeURIComponent(details)}`;
+    uri += `&completed=false`;
+    
     let td_utf8 = new TextDecoder("utf-8"); 
     fetch(uri, {method: 'POST'}).then((res) => (res.body.getReader().read()))
-    .then((res) => {todo.id = parseInt(td_utf8.decode(res.value))});
+    .then((res) => {todo.id = parseInt(td_utf8.decode(res.value));});
   }
 </script>
 
