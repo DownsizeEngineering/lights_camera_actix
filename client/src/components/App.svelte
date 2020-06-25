30d0d996acb3c4e4b3fe23313b35695bf1f57b45
Peter Shoaf <32045721+DownsizeEngineering@users.noreply.github.com>
2020-06-25 12:35:29 -0700

    move TodoList module into its own component

--- client/src/components/App.svelte
+++ client/src/components/App.svelte
@@ -1,6 +1,6 @@
 <script>
-  import List from './List.svelte';
-  import NewList from './NewList.svelte';
+  import TodoLists from './TodoLists.svelte';
+
   let string = "Hello World"
   import("../wasm/hello_wasm.js")
     .then(module => {
@@ -9,24 +9,8 @@
     }
   );
 
-  const td_utf8 = new TextDecoder("utf-8");
-
-  const newList = function(name) {
-    if (name == "") return;
-    let list = {
-      name,
-      tasks: [],
-    }
-    lists = [...lists, list]
-
-    name = name.replace(/'/g, "\\'");
-    let uri = `/list?name=${encodeURIComponent(name)}`;
-
-    fetch(uri, {method: 'POST'}).then((res) => (res.body.getReader().read()))
-    .then((res) => {list.id = parseInt(td_utf8.decode(res.value));});
-  }
-
   const fetchLists = new Promise((resolve, reject) => {
+    const td_utf8 = new TextDecoder("utf-8");
     fetch("/list").then((res) => (res.body.getReader().read()))
     .catch((error) => reject(error))
     .then((res) => resolve(JSON.parse(td_utf8.decode(res.value))))
@@ -34,15 +18,13 @@
   });
 
 </script>
+
 <p>{string}</p>
 
 {#await fetchLists}
   <p>loading lists...</p>
 {:then lists}
-  {#each lists as list}
-      <List list={list}/>
-  {/each}
-  <NewList post={newList.bind(this)}/>
+  <TodoLists lists={lists}/>
 {:catch error}
 <p>{error.message}</p>
 {/await}
---
+++ client/src/components/TodoLists.svelte
@@ -0,0 +1,10 @@
+<script>
+  import List from './List.svelte';
+  import NewList from './NewList.svelte';
+  export let lists;
+</script>
+
+{#each lists as list}
+      <List list={list}/>
+{/each}
+  <NewList bind:lists={lists}/>
