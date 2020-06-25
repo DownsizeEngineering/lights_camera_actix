ee08cbef53368d0e02831ed5f38fd358ddfe080e
Peter Shoaf <32045721+DownsizeEngineering@users.noreply.github.com>
2020-06-24 18:40:08 -0700

    move newTodo into NewTodo with bind keyword

--- client/src/components/List.svelte
+++ client/src/components/List.svelte
@@ -4,25 +4,6 @@
   import sanitizeHTML from '../Sanitize';
   export let list;
 
-  let newTodo = function(task, details) {
-    if (task === "") return;
-    let todo = {
-      task,
-      details: details ? details : "",
-    }
-    list.tasks = [...list.tasks, todo];
-
-    task = task.replace(/'/g, "\\'");
-    if (details) details = details.replace(/'/g, "\\'");
-
-    let uri = `/list/${list.id}?id=0&task=${encodeURIComponent(task)}`;
-    if (details) uri += `&details=${encodeURIComponent(details)}`;
-    uri += `&completed=false`;
-    
-    let td_utf8 = new TextDecoder("utf-8"); 
-    fetch(uri, {method: 'POST'}).then((res) => (res.body.getReader().read()))
-    .then((res) => {todo.id = parseInt(td_utf8.decode(res.value));});
-  }
   list.name = sanitizeHTML(list.name);
 </script>
 
@@ -32,4 +13,4 @@
 <Todo todo={todo}/>
 {/each}
 </ol>
-<NewTodo post={newTodo.bind(this)}/>
+<NewTodo bind:list={list}/>
--- client/src/components/NewTodo.svelte
+++ client/src/components/NewTodo.svelte
@@ -1,9 +1,30 @@
 <script>
-  export let post;
+  export let list;
   let task = "";
   let details = "";
-  const handleSubmit = function() {
-    post(task, details);
+ 
+  let newTodo = function(task, details) {
+    if (task === "") return;
+    let todo = {
+      task,
+      details: details ? details : "",
+    }
+    list.tasks = [...list.tasks, todo];
+
+    task = task.replace(/'/g, "\\'");
+    if (details) details = details.replace(/'/g, "\\'");
+
+    let uri = `/list/${list.id}?id=0&task=${encodeURIComponent(task)}`;
+    if (details) uri += `&details=${encodeURIComponent(details)}`;
+    uri += `&completed=false`;
+    
+    let td_utf8 = new TextDecoder("utf-8"); 
+    fetch(uri, {method: 'POST'}).then((res) => (res.body.getReader().read()))
+    .then((res) => {todo.id = parseInt(td_utf8.decode(res.value));});
+  }
+
+   const handleSubmit = function() {
+    newTodo(task, details);
     task = "";
     details = "";
   }
