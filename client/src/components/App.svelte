edffca9a70375cee0e82f20c900c56a173457f8e
Peter Shoaf <32045721+DownsizeEngineering@users.noreply.github.com>
2020-06-24 17:43:24 -0700

    refactor fetch list chain into fetchLists to use svelte macro

--- client/src/components/App.svelte
+++ client/src/components/App.svelte
@@ -9,21 +9,6 @@
     }
   );
 
-  let lists = [
-    {
-      "id":1,
-      "name":"shopping",
-      "tasks":[
-        {
-          "id":1,
-          "task":"eggs",
-          "details":null,
-          "completed":false
-        }
-      ]
-    }
-  ];
-
   const td_utf8 = new TextDecoder("utf-8");
 
   const newList = function(name) {
@@ -41,12 +26,22 @@
     .then((res) => {list.id = parseInt(td_utf8.decode(res.value));});
   }
 
-  fetch("/list").then((res) => (res.body.getReader().read()))
-  .then((res) => {lists = JSON.parse(td_utf8.decode(res.value));});
+  const fetchLists = new Promise((resolve, reject) => {
+    fetch("/list").then((res) => (res.body.getReader().read()))
+    .then((res) => resolve(JSON.parse(td_utf8.decode(res.value))))
+    .catch((error) => reject(error));
+  });
+
 </script>
 <p>{string}</p>
 
-{#each lists as list}
-    <List list={list}/>
-{/each}
-<NewList post={newList.bind(this)}/>
+{#await fetchLists}
+  <p>loading lists...</p>
+{:then lists}
+  {#each lists as list}
+      <List list={list}/>
+  {/each}
+  <NewList post={newList.bind(this)}/>
+{:catch error}
+<p>{error.message}</p>
+{/await}
