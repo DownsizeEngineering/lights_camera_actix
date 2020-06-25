25c5bd2256fdc48929a04dc029c872eea55ff2eb
Peter Shoaf <32045721+DownsizeEngineering@users.noreply.github.com>
2020-06-25 12:34:21 -0700

    disable blank list names, edit lists using bind:list

--- client/src/components/NewList.svelte
+++ client/src/components/NewList.svelte
@@ -1,11 +1,30 @@
 <script>
-  let newListName;
-  export let post;
+  export let lists;
+  let newListName = "";
+
   const handleSubmit = function() {
-    post(newListName);
+    newList(newListName);
     newListName = "";
   }
+
+  const newList = function(name) {
+    if (name == "") return;
+    const td_utf8 = new TextDecoder("utf-8");
+    if (name == "") return;
+    let list = {
+      name,
+      tasks: [],
+    }
+    lists = [...lists, list]
+
+    name = name.replace(/'/g, "\\'");
+    let uri = `/list?name=${encodeURIComponent(name)}`;
+
+    fetch(uri, {method: 'POST'}).then((res) => (res.body.getReader().read()))
+    .then((res) => {list.id = parseInt(td_utf8.decode(res.value));});
+  }
 </script> 
+
 <h1>New List:</h1>
 <form on:submit|preventDefault={handleSubmit}>
   <label>Name</label>
